e3f73b30a8d134667f36297b3869effc0d3ba2b2
Muhammad abdelhadi <moh.b.abdelhadi@gmail.com>
2026-07-10 22:30:17 +0400

    Update allowed web Origin

--- src/main/java/com/crm/gateway/config/ResourceServerConfig.java
+++ src/main/java/com/crm/gateway/config/ResourceServerConfig.java
@@ -31,7 +31,7 @@ public class ResourceServerConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration c = new CorsConfiguration();
-        c.setAllowedOrigins(List.of("http://localhost:5173"));
+        c.setAllowedOrigins(List.of("http://localhost:5174"));
         c.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         c.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
